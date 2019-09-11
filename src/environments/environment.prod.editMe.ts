869de02d0bdf50015bf5c76667623748d7e8d5cf
comanandrei1993 <comanandrei1993@gmail.com>
2019-09-11 13:43:57 +0300

    no message

---
+++ src/environments/environment.prod.editMe.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: 'your-data',
+    authDomain: 'your-data',
+    databaseURL: 'your-data',
+    projectId: 'your-data',
+    storageBucket: '',
+    messagingSenderId: 'your-data',
+    appId: 'your-data'
+  }
+};
